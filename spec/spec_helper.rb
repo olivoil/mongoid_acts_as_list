c3c12d3321793da9c761bd9a06e315ec5e7c943e
Olivier Melcher <olivier.melcher@gmail.com>
2012-03-17 19:58:37 +0000

    Clean up spec_helper and .vimrc.local

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,21 @@
-$: << File.expand_path('../lib', __FILE__)
 require 'rspec'
 require 'rspec/autorun'
-# require 'factory_girl'
 require 'database_cleaner'
 require 'database_cleaner/mongoid/truncation'
 
 require 'mongoid'
-require 'acts_as_list'
+require_relative '../lib/acts_as_list'
 
 Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db('acts_as_list_test')
 end
 
 RSpec.configure do |config|
-  # config.include FactoryGirl::Syntax::Methods
-
-  # focus
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
   config.before(:suite) do
     DatabaseCleaner['mongoid'].strategy = :truncation
-  end
-
-  config.after(:suite) do
   end
 
   config.before(:each) do
@@ -32,6 +24,5 @@ RSpec.configure do |config|
 
   config.after(:each) do
     DatabaseCleaner.clean
-    # Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
   end
 end
